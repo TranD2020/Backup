486f67b985070ca1ea484e5ea2ce7bdbb1f3307e
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-10-30 15:00:48 -0700

    Print numbers

--- fzbzpj.py
+++ fzbzpj.py
@@ -1,22 +1,10 @@
-num = int(input("What number to check: "))
-
-print("Checking number... ")
-
-for num in range(100):
-	count(num)
-
-if num % 3 == 0:
-	print("Your number is a multiple of 3")
-else:
-	print("Your number is not a multiple of 3")
-if num % 5 == 0:
-	print("Your number is a multiple of 5")
-else:
-	print("Your number is not a multiple of 5")
-
-
-print("Fizz")
-print("Buzz")
-
-print("Fizz", end="")
-print(" Buzz")
+count = 0
+while count < 100:
+	count += 1
+	print(count)
+	if count % 3==0:
+		print(str(count) + " Fizz")
+	if count % 5==0:
+		print(str(count) + " Buzz")
+	if count % 3 and 5==0:
+		print(str(count) + " Fizz" + " Buzz")
