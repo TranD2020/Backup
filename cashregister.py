45d4173637d44ed6068844f25adaf7ebce9bfeac
TranD2020 <56050476+TranD2020@users.noreply.github.com>
2019-11-01 15:04:14 -0700

    Messed up in lines 16 >

--- cashregister.py
+++ cashregister.py
@@ -19,21 +19,21 @@ dimes += quarters * 2.5
 nickels += dimes * 2
 pennies += nickels * 5
 
-while pennies >= 0:
-	dollars = int(pennies / 100)
-	pennies = pennies % 100
 
-	halfDollars = int(pennies / 50)
-	pennies = pennies % 50
+dollars = int(pennies / 100)
+pennies = pennies % 100
 
-	quarters = int(pennies / 25)
-	pennies = pennies % 25
+halfDollars = int(pennies / 50)
+pennies = pennies % 50
 
-	dimes = int(pennies / 10)
-	pennies = pennies % 10 
+quarters = int(pennies / 25)
+pennies = pennies % 25
 
-	nickels = int(pennies / 5)
-	pennies = pennies % 5
+dimes = int(pennies / 10)
+pennies = pennies % 10 
+
+nickels = int(pennies / 5)
+pennies = pennies % 5
 
 print("Dollars: " + dollars)
 print("Half-Dollars: " + halfDollars)
